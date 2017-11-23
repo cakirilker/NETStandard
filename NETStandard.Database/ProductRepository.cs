965ef3888eeebe8e28eb1a8615e3fa67b37fe386
Darxre <cakir.ilker329@gmail.com>
2017-11-24 00:29:56 +0300

    First commit.

---
+++ NETStandard.Database/ProductRepository.cs
@@ -0,0 +1,48 @@
+﻿using NETStandard.Shared;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NETStandard.Database
+{
+    public class ProductRepository : IRepository<Product>
+    {
+        public Task<bool> AddProductAsync(Product t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> FindByAsync(Func<Product, bool> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetAllAsync()
+        {
+            try
+            {
+                var command = @"SELECT Id, Name, Price FROM Products;";
+                return null;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public Task<Product> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateAsync(Product t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ NETStandard.Shared/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NETStandard.Shared
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public override string ToString()
+        {
+            return $"{Id} - {Name}, {Price}";
+        }
+    }
+}
